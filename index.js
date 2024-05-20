0f3e9d6a90de4e19dbe9b30e5477b03f3856f00d
daki <prokopevnazar4@gmail.com>
2024-05-20 18:54:52 +0300

    Update

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
-// const number1 = prompt ('enter first number', 2);
-// const number2 = prompt ('enter second number', 3);
+// конвертер валюти
 
-// console.log('number1 = ', number1, typeof number1);
-// console.log('number2 = ', number2, typeof number2);
+// confirm чи буде конвертація з доларів в гривні
 
-// // + - concatenation
-// const summa1 = number1 + number2; //53 - СКЛЕЄНІ РЯДКИ
-// alert ('summa1 = ' + summa1);
+// якщо так - то просимо ввести суму в доларах
 
-// const summa2 = +number1 + +number2; //8
-// alert ('summa2 = ' + summa2);
+//виводимо скільки це гривень
 
-// const summa3 =Number(number1) + Number (number2); //8
-// alert ('summa3 = ' + summa3);
+const hello = alert('Добрий день, це конвертер доларів в гривні, та гривней в долари!');
 
-// const substruct = Number(number1) - Number(number2); //2
-// alert(substruct);
-const product1 = prompt ('Назва продукту:');
-const onePrice = prompt ('Ціна продукту:');
+if (confirm('Хочете конвертувати долари в гривні?')) {
+    const howMuchDol = prompt(
+        'Введіть скільки Ви хочете конвертувати')
+        const cursDol = prompt ('Введіть курс долару')
+        const resultDol = howMuchDol*cursDol;
+        alert('Ось,' + result);
 
-const product2 = prompt ('Назва продукту:');
-const twoPrice = prompt ('Ціна продукту:');
-const allPrice = Number(onePrice) + Number(twoPrice);
+} else {
+    const change = confirm('Хочете конвертувати гривні в долари?'
 
-alert ('Ви купили:\n ' + product1 + onePrice + '\n' + product2 + twoPrice + '\n' + '-----'+ '\n' + тварини + 'Всього до сплати: ' + allPrice );
+    )
+    if (condition) {
+        const howMuchGrn = prompt(
+            'Введіть скільки Ви хочете конвертувати')
+            const cursGrn = prompt ('Введіть курс гривні')
+            const resultGrn = howMuchGrn*cursGrn;
+        alert('Ось,' + result);
+    } else {
+       alert('Добре, до побачення!');
+    }
+}
 
 
-
-//користувач послідовно вводить назву продукта і його ціну
-
-// так треба ввести інформацію про мінімум 2 продукта
-
-// вивести чек у вигляді
-
-/* Ви купили: 
-молоко - 45 грн
-хліб - 23грн 
-- - - - -
-до сплати - 68 грн
-/*
 
 
 
